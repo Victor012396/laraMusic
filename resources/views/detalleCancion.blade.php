af47f51813060af282d7578ce56712b7522fe3fb
Victor Velasco <victor.velasco5988@alumnos.udg.mx>
2023-02-13 10:31:08 -0600

    Avance project

--- resources/views/detalleCancion.blade.php
+++ resources/views/detalleCancion.blade.php
@@ -34,7 +34,7 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
@@ -50,18 +50,23 @@
                         <div class="p-6">
                             <div class="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-500"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" /></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a  class="underline text-gray-900 dark:text-white" href="/canciones">TopTrend</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a  class="underline text-gray-900 dark:text-white">{cancion['nombre']}</a></div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Conoce los 3 exitos musicales más recientes.
+                                    Conoce los exitos más recientes:
+                                    <ol>
+                                        @foreach ($canciones as $cancion)
+                                            <li>{{$cancion['nombre']}} - {{$cancion['artista']}}</li>
+                                        @endforeach
+                                    </ol>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
+                @endif
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 sm:text-left">
                         <div class="flex items-center">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/canciones/{id?}',function($id=null){
+    $canciones = [];
+    $canciones[] = ['nombre' => 'Animals', 'artista'=>'Maroon5'];
+    $canciones[] = ['nombre' => 'Girls Like You', 'artista'=>'Maroon5'];
+    $canciones[] = ['nombre' => 'Dont Wanna Know', 'artista'=>'Maroon5'];
+    //dd($canciones);
+
+    if(is_null($id)){
+        $cancion=$canciones[$id];
+    } else{
+        $cancion=null;
+    }
+
+
+    //return view('canciones')
+      //  ->with(['canciones'=>$canciones]);
+    return view('canciones', compact('canciones','cancion'));
+});
+
+//Route::get('/canciones/{id}',function($id){
+    //$canciones = [];
+    //$canciones[] = ['nombre' => 'Animals', 'artista'=>'Maroon5'];
+    //$canciones[] = ['nombre' => 'Girls Like You', 'artista'=>'Maroon5'];
+    //$canciones[] = ['nombre' => 'Dont Wanna Know', 'artista'=>'Maroon5'];
+    //dd($canciones);
+  //  $cancion = $canciones[$id];
+
+
+    //return view('canciones')
+      //  ->with(['canciones'=>$canciones]);
+    //return view('detalleCancion', compact('cancion'));
+//});
